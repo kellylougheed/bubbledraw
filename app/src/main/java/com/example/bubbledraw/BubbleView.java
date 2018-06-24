8efb7c15aa71a466748502397882b5929743a7d8
Kelly Lougheed <kellylougheed@gmail.com>
2018-06-23 22:27:49 -0700

    testBubbles pg 261

---
+++ app/src/main/java/com/example/bubbledraw/BubbleView.java
@@ -0,0 +1,48 @@
+package com.example.bubbledraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BubbleView extends ImageView implements View.OnTouchListener {
+    private Random rand = new Random();
+    private ArrayList<Bubble> bubbleList;
+    private int size = 50;
+    private int delay = 33;
+    private Paint myPaint = new Paint();
+    private Handler h = new Handler();
+
+    public BubbleView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        bubbleList = new ArrayList<Bubble>();
+        testBubbles();
+    }
+
+    protected void onDraw(Canvas canvas) {
+        for (Bubble b : bubbleList) {
+            b.draw(canvas);
+        }
+    }
+
+    public void testBubbles() {
+        for (int i = 0; i < 100; i++) {
+            int x = rand.nextInt(600);
+            int y = rand.nextInt(600);
+            int s = rand.nextInt(size) + size;
+            bubbleList.add(new Bubble(x, y, s));
+        }
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+}
